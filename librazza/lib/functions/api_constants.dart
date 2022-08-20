d42b6d86873427927e0fdd20354a3f55f5331c72
Valdemir <valdemireverton08@gmail.com>
2022-08-20 11:10:43 -0300

    create

---
+++ librazza/lib/functions/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  static String baseUrl = "https://127.0.0.1";
+  static String authorEndpoint = "/autor";
+  static String booksEndpoint = "/livro";
+  static String customersEndpont = "/cliente";
+  static String employeesEndpoint = "/funcionario";
+  static String loansEndpoint = "/emprestimo";
+}
